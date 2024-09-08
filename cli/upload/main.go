41a20a2798f8752b63f09fcf8777a827bf90f16d
Jamie Emerson <inghamemerson@gmail.com>
2024-09-07 20:24:22 -0700

    minor updates

--- cli/upload/main.go
+++ cli/upload/main.go
@@ -28,14 +28,15 @@ type S3ClientConfig struct {
 var uploadFiles = make(chan string)
 var uploadWg = new(sync.WaitGroup)
 var uploadBucket = "neuroscan-files"
-var directoryIgnore = "/Users/inghamemerson/Code/intralab/neuroscan/backend/public/"
-var uploadDirectory = "/Users/inghamemerson/Code/intralab/neuroscan/backend/public/files/neuroscan"
+var directoryIgnore = "/Users/inghamemerson/Sync/to_upload/"
+var uploadDirectory = "/Users/inghamemerson/Sync/to_upload/files/neuroscan"
 var bucketFolder string
 var validExtensions = []string{".gltf"}
 
 func main() {
 	// bucket folder is a string representing the datetime of the upload
-	bucketFolder = time.Now().Format("2006_01_02T15_04_05")
+	//bucketFolder = time.Now().Format("2006_01_02T15_04_05")
+	bucketFolder = "2024_08_21T14_00_28"
 	processUploadDirectory(uploadDirectory)
 }
 
